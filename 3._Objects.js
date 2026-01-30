43754c5a90be7497aa296751604271cd6db7a143
shuresm57 <vsorensen31@gmail.com>
2026-01-30 15:37:34 +0100

    3._Objects færdig

--- 3._Objects.js
+++ 3._Objects.js
@@ -7,11 +7,19 @@ const greetings = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
+console.log(greetings.message)
+
 // --------------------------------------
 // Exercise 2 - Defining an object.
 
 // Create an object that has your name and age. 
 
+const me = { 
+    name: "Valdemar",
+    age: 31
+}
+
+console.log(me)
 
 // --------------------------------------
 // Exercise 3 - Add a key-value pair
@@ -20,6 +28,10 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
+console.log(stackOverflow)
+
 // --------------------------------------
 // Exercise 4 - Remove a property
 
@@ -27,6 +39,11 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.description = "Just a tribute"
+thisSong.angels = false
+thisSong.butMen = true;
+console.log(thisSong)
 
 // --------------------------------------
 
