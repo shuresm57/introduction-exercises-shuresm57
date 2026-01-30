c059f00f0d135a3e37c0dc84d136587c90646f32
shuresm57 <vsorensen31@gmail.com>
2026-01-30 15:29:43 +0100

    2._Strings færdig

--- 2._Strings.js
+++ 2._Strings.js
@@ -8,14 +8,15 @@ const numberTwo = "2.30";
 // you cannot touch line 1 neither line 2
 
 const numberThree = parseFloat(numberOne) + parseFloat(numberTwo);
-
+console.log(numberThree);
 // --------------------------------------
 // Exercise 4 - Add the numbers and the total with 2 decimals
 
 const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
 
-
+const anotherNumberThree = Number(anotherNumberOne) + Number(anotherNumberTwo);
+console.log(anotherNumberThree.toFixed(2));
 // --------------------------------------
 // Exercise 5 - Decimals and average
 
@@ -25,8 +26,9 @@ const three = 98;
 
 // Show in the console the avg. with 5 decimals
 
-
-
+const numberArray = [one, two, three]
+const avg = one + two + three / numberArray.length
+console.log(avg.toFixed(5))
 
 // --------------------------------------
 // Exercise 6 - Get the character by index
@@ -34,7 +36,8 @@ const three = 98;
 const letters = "abc";
 // Get me the character "c"
 
-
+console.log(letters.charAt(2))
+console.log(letters[2])
 
 
 // --------------------------------------
@@ -44,6 +47,9 @@ const fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
 
+const capitalizedJavascript = fact.replace("javascript", "Javascript")
+// Eller fact.replace("j", "J") men hvis der andre j'er i String bliver kun den første capitalized
+console.log(capitalizedJavascript)
 
 // --------------------------------------
 
